3660c0683704bbd4daece4c86b325c625638c006
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-10-04 04:21:00 +0500

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:player_ai/screens/splash_screen.dart';
 
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
 
-void main() {
+import 'package:musicxy/paywall_screen.dart';
+import 'package:musicxy/routes/app_pages.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+
+  print('Running app...');
   runApp(const MyApp());
 }
 
@@ -11,14 +22,67 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Player AI',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return Sizer(
+      builder: (context, orientation, deviceType) => MediaQuery(
+        data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+        child: GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: const PaywallScreen(),
+          theme: ThemeData(
+            brightness: Brightness.dark,
+            scaffoldBackgroundColor: Colors.black,
+            primaryColor: Colors.black,
+            colorScheme: ColorScheme.dark(
+              primary: Colors.white,
+              secondary: Colors.black,
+              background: Colors.black,
+              surface: Colors.grey[900]!,
+              onPrimary: Colors.white,
+              onSecondary: Colors.white,
+              onBackground: Colors.white,
+              onSurface: Colors.white,
+            ),
+            cardColor: Colors.grey[900]!,
+            dialogBackgroundColor: Colors.grey[900]!,
+            appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.black,
+              elevation: 0,
+              iconTheme: IconThemeData(color: Colors.white),
+              titleTextStyle: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+              backgroundColor: Colors.black,
+              selectedItemColor: Colors.white,
+              unselectedItemColor: Colors.white70,
+              elevation: 8,
+            ),
+            inputDecorationTheme: InputDecorationTheme(
+              filled: true,
+              fillColor: Colors.white10,
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(16)),
+                borderSide: BorderSide.none,
+              ),
+              hintStyle: const TextStyle(color: Colors.white54),
+            ),
+            textTheme: GoogleFonts.urbanistTextTheme(
+              ThemeData.dark().textTheme,
+            ).apply(bodyColor: Colors.white, displayColor: Colors.white),
+            iconTheme: const IconThemeData(color: Colors.white),
+            dividerColor: Colors.white12,
+            shadowColor: Colors.black54,
+            splashColor: Colors.white24,
+            highlightColor: Colors.white10,
+          ),
+          builder: EasyLoading.init(),
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+        ),
       ),
-      home: const SplashScreen(),
     );
   }
 }
