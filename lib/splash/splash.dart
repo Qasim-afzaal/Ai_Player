c52d050dabaed74a8d17c0ef6fac41a4e4a900f1
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-10-13 04:29:57 +0500

    make ui for splash

---
+++ lib/splash/splash.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get_state_manager/src/simple/get_state.dart';
+import 'package:musicxy/services/analytics_helper.dart';
+
+import 'splash_controller.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    AnalyticsHelper.logScreenView('SplashScreen');
+    return GetBuilder(
+      init: SplashController(),
+      builder: (controller) {
+        return Scaffold(
+          body: Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+            ),
+            child: Center(
+              child: Image.asset(
+                "assets/images/logo.png",
+                scale: 2,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
