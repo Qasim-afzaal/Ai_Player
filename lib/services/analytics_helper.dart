b0871b2262d3d10de5754604ecd04da58576f2d6
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-10-13 04:28:56 +0500

    handle analytics

---
+++ lib/services/analytics_helper.dart
@@ -0,0 +1,19 @@
+class AnalyticsHelper {
+  static Future<void> logScreenView(String screenName) async {
+    // Analytics disabled
+  }
+
+  static Future<void> logButtonTap(
+    String buttonName, {
+    String? screenName,
+  }) async {
+    // Analytics disabled
+  }
+
+  static Future<void> logCustomEvent(
+    String eventName, {
+    Map<String, Object?>? parameters,
+  }) async {
+    // Analytics disabled
+  }
+}
